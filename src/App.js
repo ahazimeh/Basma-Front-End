51d20afebe4e21a82bf7ea02089956f45266bf0b
ali-pixel38 <ali@pixel38.com>
2021-09-16 23:42:26 +0300

    code challenge part

--- src/App.js
+++ src/App.js
@@ -4,17 +4,95 @@ import './App.scss';
 function App() {
   return (
     <div className="App">
-      <div className="left">sssssssssssssssss</div>
-      <div className="right">
-        <div class="box"></div>
-        <img className="clip-phone" src="/Images/images/thumb-1.png" />
-        <div className="nav-bar">
-          <div>Home</div>
-          <div>Features</div>
-          <div>Pages</div>
-          <div>Screenshots</div>
-          <div>Pricing</div>
-          <div>Contact</div>
+      <div className="header-container">
+        <div className="left">
+          <div className="abs">
+            <div className="title">
+              Creative way to showcase your App
+            </div>
+            <div className="sub-title">
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+              <input type="button" className="button" value="Get Started" />
+            </div>
+          </div>
+        </div>
+        <div className="right">
+          <div class="box"></div>
+          <img className="clip-phone" src="/Images/images/thumb-1.png" />
+          <div className="nav-bar">
+            <div>Home</div>
+            <div>Features</div>
+            <div>Pages</div>
+            <div>Screenshots</div>
+            <div>Pricing</div>
+            <div>Contact</div>
+          </div>
+        </div>
+      </div>
+      <div className="code-challenge">
+        <div>
+          <div className="title">Code Challenge</div>
+          <hr color="blue" className="hr" />
+          <p className="sub-title">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod</p>
+          <div className="code-challenge-body">
+            <div className="element">
+              <div className="element-center">
+                <img src="/svg/pointer-top.svg" />
+              </div>
+              <div className="element-title">Fully functional</div>
+              <div className="element-center">
+                <p className="element-content">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod</p>
+              </div>
+            </div>
+            <div className="element">
+              <div className="element-center">
+                <img src="/svg/pointer-top.svg" />
+              </div>
+              <div className="element-title">Fully functional</div>
+              <div className="element-center">
+                <p className="element-content">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod</p>
+              </div>
+            </div>
+            <div className="element">
+              <div className="element-center">
+                <img src="/svg/pointer-top.svg" />
+              </div>
+              <div className="element-title">Fully functional</div>
+              <div className="element-center">
+                <p className="element-content">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod</p>
+              </div>
+            </div>
+          </div>
+          <div className="code-challenge-body">
+            <div className="element">
+              <div className="element-center">
+                <img src="/svg/pointer-top.svg" />
+              </div>
+              <div className="element-title">Fully functional</div>
+              <div className="element-center">
+                <p className="element-content">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod</p>
+              </div>
+            </div>
+            <div className="element">
+              <div className="element-center">
+                <img src="/svg/pointer-top.svg" />
+              </div>
+              <div className="element-title">Fully functional</div>
+              <div className="element-center">
+                <p className="element-content">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod</p>
+              </div>
+            </div>
+            <div className="element">
+              <div className="element-center">
+                <img src="/svg/pointer-top.svg" />
+              </div>
+              <div className="element-title">Fully functional</div>
+              <div className="element-center">
+                <p className="element-content">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod</p>
+              </div>
+            </div>
+          </div>
+
         </div>
       </div>
     </div>
