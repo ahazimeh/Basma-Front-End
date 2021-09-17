3dffb6aeef493e92921e80cda2eb52a69c02818f
ali-pixel38 <ali@pixel38.com>
2021-09-17 08:20:54 +0300

    reached products section

--- src/App.js
+++ src/App.js
@@ -34,65 +34,177 @@ function App() {
           <div className="title">Code Challenge</div>
           <hr color="blue" className="hr" />
           <p className="sub-title">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod</p>
-          <div className="code-challenge-body">
-            <div className="element">
-              <div className="element-center">
-                <img src="/svg/pointer-top.svg" />
-              </div>
-              <div className="element-title">Fully functional</div>
-              <div className="element-center">
-                <p className="element-content">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod</p>
-              </div>
+
+
+        </div>
+      </div>
+      <div className="code-challenge-body">
+        <div className="element">
+          <div className="element-center">
+            <img src="/svg/pointer-top.svg" />
+          </div>
+          <div className="element-title">Fully functional</div>
+          <div className="element-center">
+            <p className="element-content">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod</p>
+          </div>
+        </div>
+        <div className="element">
+          <div className="element-center">
+            <img src="/svg/pointer-top.svg" />
+          </div>
+          <div className="element-title">Fully functional</div>
+          <div className="element-center">
+            <p className="element-content">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod</p>
+          </div>
+        </div>
+        <div className="element">
+          <div className="element-center">
+            <img src="/svg/pointer-top.svg" />
+          </div>
+          <div className="element-title">Fully functional</div>
+          <div className="element-center">
+            <p className="element-content">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod</p>
+          </div>
+        </div>
+      </div>
+      <div className="code-challenge-body">
+        <div className="element">
+          <div className="element-center">
+            <img src="/svg/pointer-top.svg" />
+          </div>
+          <div className="element-title">Fully functional</div>
+          <div className="element-center">
+            <p className="element-content">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod</p>
+          </div>
+        </div>
+        <div className="element">
+          <div className="element-center">
+            <img src="/svg/pointer-top.svg" />
+          </div>
+          <div className="element-title">Fully functional</div>
+          <div className="element-center">
+            <p className="element-content">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod</p>
+          </div>
+        </div>
+        <div className="element">
+          <div className="element-center">
+            <img src="/svg/pointer-top.svg" />
+          </div>
+          <div className="element-title">Fully functional</div>
+          <div className="element-center">
+            <p className="element-content">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod</p>
+          </div>
+        </div>
+      </div>
+
+      <div className="work-faster-wrapper">
+        <div className="work-faster">
+          <img src="/Images/images/thumb-2 (1).png" />
+          <div className="work-faster-content">
+            <div className="work-faster-title">Work faster with powerful tools.</div>
+            <div className="work-faster-text">
+              <p><img src="/svg/arrow-right.svg" />Combined with a handful of model sentence structures looks reasonable</p>
+              <p><img src="/svg/arrow-right.svg" />Contrary to popular belief, Lorem lpsum is not simply random text.</p>
+              <p><img src="/svg/arrow-right.svg" />Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium.</p>
+              <p><img src="/svg/arrow-right.svg" />Natus error sit voluptatem unde amnis iste natus error sit voluptatem accusantium</p>
+              <p><img src="/svg/arrow-right.svg" />All the Lorem lpsum generators on the internet tend to repeat necessary.</p>
             </div>
-            <div className="element">
-              <div className="element-center">
-                <img src="/svg/pointer-top.svg" />
-              </div>
-              <div className="element-title">Fully functional</div>
-              <div className="element-center">
-                <p className="element-content">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod</p>
-              </div>
-            </div>
-            <div className="element">
-              <div className="element-center">
-                <img src="/svg/pointer-top.svg" />
-              </div>
-              <div className="element-title">Fully functional</div>
-              <div className="element-center">
-                <p className="element-content">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod</p>
-              </div>
+            <div className="icons">
+              <img src="/svg/facebook-messenger.svg" />
+              <img src="/svg/lifering.svg" />
+              <img src="/svg/map-marker.svg" />
             </div>
           </div>
-          <div className="code-challenge-body">
-            <div className="element">
-              <div className="element-center">
-                <img src="/svg/pointer-top.svg" />
-              </div>
-              <div className="element-title">Fully functional</div>
-              <div className="element-center">
-                <p className="element-content">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod</p>
-              </div>
-            </div>
-            <div className="element">
-              <div className="element-center">
-                <img src="/svg/pointer-top.svg" />
-              </div>
-              <div className="element-title">Fully functional</div>
-              <div className="element-center">
-                <p className="element-content">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod</p>
-              </div>
-            </div>
-            <div className="element">
-              <div className="element-center">
-                <img src="/svg/pointer-top.svg" />
-              </div>
-              <div className="element-title">Fully functional</div>
-              <div className="element-center">
-                <p className="element-content">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod</p>
-              </div>
-            </div>
+        </div>
+      </div>
+      <div className="share-photos">
+        <div className="share-photos-left-section">
+          <div className="share-title">Share your photos with friends easily</div>
+          <div>
+            <p className="share-photo-content">
+              <img src="/svg/cart.svg" />
+              Fully layered dolor sit amet, consectetur elit. Facere, nobis, id expedita dolores officiis laboriosam.
+            </p>
+            <p className="share-photo-content">
+              <img src="/svg/cart.svg" />
+              Customizable design dolor sit amet, consectetur adipisicing eli, nobis, id expedita dolores officiis la.
+            </p>
+            <p className="share-photo-content">
+              <img src="/svg/cart.svg" />
+              Fully layered dolor sit amet, consectetur elit. Facere, nobis, id expedita dolores officiis laboriosam.
+            </p>
+            <p className="share-photo-content">
+              <img src="/svg/cart.svg" />
+              Marketing chart dolor sit amet, consecetetur adipisicing elit. Facere, nobis, id expedita dolores officiis, laboriosam.
+            </p>
           </div>
 
+          <input type="button" className="button" value="Get Started" />
+
+        </div>
+        <div className="share-phone">
+          <img src="/Images/images/thumb-2.png" />
+        </div>
+      </div>
+      <div className="challenge-tips">
+        <div className="challenge-tips-header">
+          <p className="challenge-tips-title">Code Challenge</p>
+          <p className="challenge-tips-sub-title">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore</p>
+          <p className="challenge-tips-sub-title">et dolore magna aliqua. Ut enim ad minim veniam</p>
+        </div>
+        <div className="challenge-tips-body-wrapper">
+          <div className="challenge-tips-body">
+            <div className="challenge-tips-body-element">
+              <div className="flex">
+                <div>
+                  <img src="/svg/cloud-download.svg" />
+                  <p>Install the App</p>
+                  <p>dsadasd sdfsd fssd sdf sd</p>
+                </div>
+                <div>
+                  <img className="arrow" src="/svg/arrow-right.svg" />
+                </div>
+              </div>
+            </div>
+            <div className="challenge-tips-body-element">
+              <div className="flex">
+                <div>
+                  <img src="/svg/cloud-download.svg" />
+                  <p>Install the App</p>
+                  <p>dsadasd sdfsd fssd sdf sd</p>
+                </div>
+                <div>
+                  <img className="arrow" src="/svg/arrow-right.svg" />
+                </div>
+              </div>
+            </div>
+            <div className="challenge-tips-body-element">
+              <img src="/svg/cloud-download.svg" />
+              <p>Install the App</p>
+              <p>dsadasd sdfsd fssd sdf sd</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="products">
+        <div className="products-header">
+          <span className="products-title">Basma Code Challenge</span>
+        </div>
+        <hr color="blue" className="hr" />
+        <div className="products-header-sub">
+          <span className="products-sub-title">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nis</span>
+        </div>
+        <div className="items">
+          <div className="single-item">
+            <div className="single-item-container">
+              <img src="/svg/bi-cycle.svg" />
+            </div>
+          </div>
+          <div className="single-item">
+            <div className="single-item-container">
+              <img src="/svg/bi-cycle.svg" />
+            </div>
+          </div>
         </div>
       </div>
     </div>
